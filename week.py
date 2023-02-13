f1bc51d669284e9a6e39f6c1006b7fe78e62e095
dishu81 <119942547+dishu81@users.noreply.github.com>
2023-02-13 15:54:41 +0530

    Add files via upload

---
+++ week.py
@@ -0,0 +1,28 @@
+
+n=int(input("enter the number :"))
+for i in range(1,n+1):	
+
+	if i==1:
+		print("1 - monday")
+
+	elif i==2:
+		print("2 - tuesday")
+
+	elif i==3:
+		print("3 - wednesday")
+
+	elif i==4:
+		print("4 - thursday")
+
+	elif i==5:
+		print("5 - friday")
+
+	elif i==6:
+		print("6 - saturday")
+
+	elif i==7:
+		print("7 - sunday")
+
+	else:
+		print("weekdays")
+
